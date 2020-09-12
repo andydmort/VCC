6fd3411f69cb12c97d0259c5c13066a040a83858
andydmort <andydmort@gmail.com>
2020-09-11 19:29:30 -0700

    Started on some example templates. Also started on the compiler, if I can call it that, algorithm.

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+
+
+
+// Look in template directory and make (at least) a list of the templates
+
+    // look at the html
+    // take out the style and link tags for style  
+    // save those to include in master
+    // take out the js and save for later
+    // create the graph nodes
+
+
+
+
+// create a template tree and figure out the order to place the templates in a file
+
+// Start the injection, one file at a time
+
