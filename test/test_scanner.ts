1fc52aa592d24dccbf41d59505abeaffe7e5a811
andydmort <andydmort@gmail.com>
2020-09-26 12:23:45 -0700

    chore: starting with some tests.

---
+++ test/test_scanner.ts
@@ -0,0 +1,11 @@
+
+
+import { decodedTextSpanIntersectsWith, idText } from "typescript";
+import {File_Scanner} from "../src/scanner";
+
+
+describe("Scanner", ()=>{
+    it( "Should be able to can in a simple file", ()=>{
+        console.log("printing")
+    });
+});
