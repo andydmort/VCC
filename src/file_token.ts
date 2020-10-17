d8ed2084b4f13a16a5d1b9d505b05ffc58e1de5c
andydmort <andydmort@gmail.com>
2020-10-17 07:36:44 -0700

    Added some command line arguments and some fulfillment functions to the file token.

--- src/file_token.ts
+++ src/file_token.ts
@@ -17,21 +17,26 @@ interface an_import
     group_import_name: string // groupt name that has the code to import
     group_name: string, // The group that contains the import statement
     line_number: number, // The line number of the import statement in the group
+    fulfilled: boolean
 };
 
 export class File_Token {
 
-    name: string;
+    name: string; 
     groups: Map<string,string[]>; // Group name to array of strings
     // import name to 
-    imports: Map<string, an_import[]>; // file id import -> an_import object.
+    imports: Map<string, an_import[]>; // import_file_name -> array of an_imports
+    /**
+     * Specifies if the File_Token has been completed fulfilled.
+     */
+    fulfilled: boolean;
 
     constructor(name_:string){
         this.name = name_;
         this.groups = new Map<string, string[]>();
         this.imports = new Map<string, an_import[]>();
+        this.fulfilled = false;
     }
-
 
     add_group(name: string, lines: string[]){
         this.groups.set(name, lines);
@@ -48,7 +53,8 @@ export class File_Token {
                 file_import_name:import_file_name, 
                 group_import_name: import_group_name,
                 line_number:line_number, 
-                group_name:group_name
+                group_name:group_name,
+                fulfilled: false
             });
     }
 
@@ -60,5 +66,68 @@ export class File_Token {
         return this.imports;
     }
 
+    /**
+     * Get an import to be fulfilled
+     */
+    get_next_import(): [string, an_import[]]{
+        if(this.imports.size > 0)
+        {
+            let key =Array.from(this.imports.keys())[0]; 
+            return [key, this.imports.get(key)];
+        }
+    }
 
+    /**
+     * Looks at all the import statements and marks this token as fulfilled if all imports
+     * were fulfilled.
+     */
+    check_if_fulfilled(){
+        for(let import_arr_key in Array.from(this.imports))
+        {
+            for(let thang of this.imports.get(import_arr_key))
+            {
+                if(!thang.fulfilled)
+                    return;
+            }
+        }
+        this.fulfilled = true;
+    }
+
+    /**
+     * Fulfills an import.
+     * @param the_import Specifies the import to be fulfilled.
+     * @param lines Specifes the lines that will be place directly under import statement. 
+     * @return True if something was fulfilled.
+     */
+    fulfill_import(the_import: an_import, lines: string[]): boolean
+    {
+        // Replace the lines just under the line number
+        let line_number: number = the_import.line_number;
+        let group: string = the_import.group_name;
+
+        
+        // Fulfil this in the imports
+        for(let thang of this.imports.get(the_import.file_import_name))
+        {
+            if(thang.group_import_name == the_import.group_import_name &&
+                thang.group_name == the_import.group_name &&
+                thang.line_number == the_import.line_number)
+            {
+                // Fulfill this in the group
+                this.groups.get(group).splice(line_number+1, 0, ...lines);
+                thang.fulfilled = true;            
+                this.check_if_fulfilled();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Gets if the token is fulfilled
+     */
+    is_fulfiled(): boolean{
+        return this.fulfilled;
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,61 @@
 import {File_Token} from "./file_token";
 import {File_Scanner, Main_File_Scanner} from "./scanner"
 import path from "path";
+import yargs from "yargs";
 
-let scanner = new File_Scanner(path.join(__dirname,"..", "example_templates", "test2.html"));
-scanner.scan();
-console.log(scanner.file_token);
-let m_scanner = new Main_File_Scanner(path.join(__dirname,"..", "example_templates", "test2.html"));
-m_scanner.scan();
-console.log(m_scanner.file_token);
+let argv = yargs(process.argv.slice(2))
+    .usage(`Usage: $0 [options]`)
+
+    .describe('m', 'Sets the main file')
+    .alias('m', "main_file")
+    .default('m', "main.html")
+
+    .describe("o", "resulting file")
+    .alias("o", "output_file")
+    .default("o","result.html")
+
+    .describe('s', 'path to source directory')
+    .alias("s", "src")
+    .alias("s", "source")
+    .demandOption("s")
+
+    .help('h')
+    .alias("h", "help")
+    .argv;
+
+console.log(argv.m);
+
+
+// parse main file
+let main_scanner: Main_File_Scanner = new Main_File_Scanner(path.join(String(argv.s), argv.m));
+let main_token = main_scanner.scan();
+// Fulfill whats needed.
+
+console.log(main_token.get_groups());
+console.log(main_token.get_imports());
+
+
+
+
+
+
+// put to output file
+
+
+
+// let scanner = new File_Scanner(path.join(__dirname,"..", "example_templates", "test2.html"));
+// scanner.scan();
+// console.log(scanner.file_token);
+// let m_scanner = new Main_File_Scanner(path.join(__dirname,"..", "example_templates", "test2.html"));
+// m_scanner.scan();
+// console.log(m_scanner.file_token);
+
+
+// Start with a passed in main file
+
+
+
+
 
 // Look in template directory and make (at least) a list of the templates
 
